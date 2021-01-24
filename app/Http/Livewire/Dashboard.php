42c0c0e480b8168b280b38def767d7e7f52ecfc1
luisurruela <luis.urruela@gmail.com>
2021-01-24 12:06:01 -0600

    Adding CRUD Modals

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -6,8 +6,15 @@ use Livewire\Component;
 
 class Dashboard extends Component
 {
+    public $showCreateModal = false;
+
     public function render()
     {
         return view('livewire.dashboard');
+    }
+
+    public function create()
+    {
+        $this->showCreateModal = true;
     }
 }
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -3,9 +3,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="p-6">
-                    <h1>Dashboard component</h1>
+                    <x-jet-button class="ml-2" wire:click="create">
+                        {{ __('Nueva apuesta') }}
+                    </x-jet-button>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+@include('livewire.modals')
